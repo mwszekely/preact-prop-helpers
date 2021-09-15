9d664487ce560a20deed9d210a86d4d1ed96f93b
mwszekely <19625841+mwszekely@users.noreply.github.com>
2021-09-15 19:49:56 -0400

    Made more types & functions public

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ export { useElementSize, UseElementSizeReturnType } from "./use-element-size";
 export { useLogicalDirection, PhysicalDirection, LogicalDirectionInfo, PhysicalSize, PhysicalOrientation, LogicalOrientation, LogicalElementSize } from "./use-logical-direction";
 export { useMediaQuery } from "./use-media-query";
 export { useMergedClasses } from "./use-merged-classes";
-export { useMergedProps } from "./use-merged-props";
+export { useMergedProps, MergedProps } from "./use-merged-props";
 export { useMergedRefs } from "./use-merged-refs";
 export { useMergedStyles } from "./use-merged-styles";
 export { useRefElement, UseRefElementPropsReturnType, UseRefElementReturnType, UseRefElementPropsParameters } from "./use-ref-element";
@@ -19,12 +19,14 @@ export { useHasFocus } from "./use-has-focus";
 export { useActiveElement } from "./use-active-element";
 export { useInterval } from "./use-interval";
 export { usePersistentState } from "./use-persistent-state";
-export { useRandomId } from "./use-random-id";
+export { useRandomId, UseRandomIdPropsReturnType, UseReferencedIdPropsReturnType } from "./use-random-id";
 export { useTimeout } from "./use-timeout";
-export { useChildManager, ManagedChildInfo } from "./use-child-manager";
+export { useChildManager, useChildFlag, ManagedChildInfo } from "./use-child-manager";
 export { useRovingTabIndex, UseRovingTabIndexChild, UseRovingTabIndexReturnType, RovingTabIndexChildInfo, UseRovingTabIndexChildPropsReturnType, UseRovingTabIndexChildReturnType, UseRovingTabIndexParameters } from "./use-roving-tabindex";
-export { useListNavigation, UseListNavigationChild, UseListNavigationChildPropsReturnType, UseListNavigationChildReturnType, UseListNavigationParameters, UseListNavigationReturnType } from "./use-list-navigation"
+export { useListNavigation, UseListNavigationChild, UseListNavigationChildProps, UseListNavigationChildPropsReturnType, UseListNavigationChildReturnType, UseListNavigationParameters, UseListNavigationReturnType, UseListNavigationChildInfo, UseListNavigationChildParameters } from "./use-list-navigation"
 
-export { useFocusTrap, UseFocusTrapReturnType, UseFocusTrapParameters, UseFocusTrapPropsParameters, UseFocusTrapPropsReturnType } from "./use-focus-trap";
+export { useEffect } from "./use-effect";
+export { useLayoutEffect } from "./use-layout-effect";
+export { useFocusTrap, findFirstFocusable, UseFocusTrapReturnType, UseFocusTrapParameters, UseFocusTrapPropsParameters, UseFocusTrapPropsReturnType } from "./use-focus-trap";
 
 
