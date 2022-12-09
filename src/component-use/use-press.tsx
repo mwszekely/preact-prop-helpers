929d908acd3f5dbe08d7f6be0f4f3aca882fce20
mwszekely <19625841+mwszekely@users.noreply.github.com>
2022-12-09 12:16:17 -0500

    Ignore programmatic clicks from radio checked

--- src/component-use/use-press.tsx
+++ src/component-use/use-press.tsx
@@ -317,7 +317,16 @@ export function usePress<E extends Element>(args: UsePressParameters<E>): UsePre
                 // events that don't immediately follow a bunch of pointer and mouse events
                 // and also was fired **specifically** on this element.
                 // (That second check is to avoid bubbled clicks being caught as programmatic presses on parent components)
-                if (getJustHandled() == false && e.target == element) {
+                if (
+                    // Ignore click events that were just handled with pointerup
+                    getJustHandled() == false &&
+                    // Ignore click events that were't fired SPECIFICALLY on this element
+                    e.target == element &&
+                    // Ignore click events that were fired on a checked radio
+                    // (Whenever the `checked` property is changed, all browsers fire a `click` event, no matter the reason for the change,
+                    // but since *we* were the reason for the change, this will always be a duplicate event related to whatever we just did.)
+                    element?.tagName == 'input' && (element as Element as HTMLInputElement).type == 'radio' && (element as Element as HTMLInputElement).checked
+                ) {
                     // Intentional, for now. Programmatic clicks shouldn't happen in most cases.
                     // TODO: Remove this when I'm confident stray clicks won't be handled.
                     console.log(false);
