fee5139356cac2bc537dbeec197404db31e4a2b8
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-02-14 23:27:26 -0500

    Minor change to `useMediaQuery`

--- src/observers/use-media-query.tsx
+++ src/observers/use-media-query.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useRef } from "preact/hooks";
+import { useLayoutEffect, useRef } from "preact/hooks";
 import { useState } from "../preact-extensions/use-state";
 
 export function useMediaQuery(query: string | null | undefined) {
-    const mediaList = useRef<MediaQueryList | null>(null);
     const [matches, setMatches, getMatches] = useState(false);
 
     console.assert(!query || query.startsWith("("));
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (!query)
             return;
 
-        mediaList.current = matchMedia(query);
+        const queryList = matchMedia(query);
+        setMatches(queryList.matches);
 
         const handler = (e: MediaQueryListEvent) => {
             setMatches(e.matches);
         }
 
-        mediaList.current.addEventListener("change", handler);
-        return () => mediaList.current?.removeEventListener("change", handler);
+        queryList.addEventListener("change", handler, { passive: true });
+        return () => queryList.removeEventListener("change", handler);
 
     }, [query]);
 
