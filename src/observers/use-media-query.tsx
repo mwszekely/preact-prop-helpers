db75016629396dc2092c2c09043f540d4223aca6
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-02-15 00:10:38 -0500

    Improve `useMediaQuery` performance on mount

--- src/observers/use-media-query.tsx
+++ src/observers/use-media-query.tsx
@@ -2,7 +2,9 @@ import { useLayoutEffect, useRef } from "preact/hooks";
 import { useState } from "../preact-extensions/use-state";
 
 export function useMediaQuery(query: string | null | undefined) {
-    const [matches, setMatches, getMatches] = useState(false);
+    const queryList = useRef<MediaQueryList | null>();
+    queryList.current ??= (query == null ? null : matchMedia(query))
+    const [matches, setMatches, getMatches] = useState(queryList.current?.matches || false);
 
     console.assert(!query || query.startsWith("("));
 
@@ -10,15 +12,15 @@ export function useMediaQuery(query: string | null | undefined) {
         if (!query)
             return;
 
-        const queryList = matchMedia(query);
-        setMatches(queryList.matches);
+        queryList.current = matchMedia(query);
+        setMatches(queryList.current.matches || false);
 
         const handler = (e: MediaQueryListEvent) => {
             setMatches(e.matches);
         }
 
-        queryList.addEventListener("change", handler, { passive: true });
-        return () => queryList.removeEventListener("change", handler);
+        queryList.current.addEventListener("change", handler, { passive: true });
+        return () => queryList.current?.removeEventListener("change", handler);
 
     }, [query]);
 
