d6de4c0ab55aa3f97b8208cdd3f7333676f8a504
mwszekely <19625841+mwszekely@users.noreply.github.com>
2026-02-11 22:46:23 -0500

    Correct JSX namespace

--- dist/preact/util/lib-preact.js
+++ dist/preact/util/lib-preact.js
@@ -1,9 +1,9 @@
-export { cloneElement, createContext, createElement, Fragment } from "preact";
-export { createPortal, forwardRef, memo } from "preact/compat";
-export { useContext, useDebugValue, useEffect, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from "preact/hooks";
 import { options } from "preact";
 import { useCallback as ucb } from "preact/hooks";
 import { EventMapping as em } from "./lib-shared.js";
+export { cloneElement, createContext, createElement, Fragment } from "preact";
+export { createPortal, forwardRef, memo } from "preact/compat";
+export { useContext, useDebugValue, useEffect, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from "preact/hooks";
 // Patch the type (only the type) of useCallback to allow for nullable functions
 export const useCallback = ucb;
 // In React, this is useInsertionEffect
--- src/util/lib-preact.tsx
+++ src/util/lib-preact.tsx
@@ -1,13 +1,17 @@
+import { options } from "preact";
+import { Dispatch, Inputs, StateUpdater as SU, useCallback as ucb } from "preact/hooks";
+import type { JSX } from "preact/jsx-runtime";
+import { EventMapping as em } from "./lib-shared.js";
+
+
 export { cloneElement, createContext, createElement, Fragment, type ComponentChildren, type ComponentType, type Context } from "preact";
-export type { JSX, Ref, RefCallback, RenderableProps } from "preact";
+export type { Ref, RefCallback, RenderableProps } from "preact";
 export { createPortal, forwardRef, memo } from "preact/compat";
 export { useContext, useDebugValue, useEffect, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from "preact/hooks";
 export type { EffectCallback, Inputs, MutableRef, Reducer } from "preact/hooks";
+export type { JSX } from "preact/jsx-runtime";
 export type { ExtendMerge, Nullable, OmitStrong, TargetedOmit, TargetedPick } from "./lib-shared.js";
-import type { JSX } from "preact";
-import { options } from "preact";
-import { Dispatch, Inputs, StateUpdater as SU, useCallback as ucb } from "preact/hooks";
-import { EventMapping as em } from "./lib-shared.js";
+
 
 // Patch the type (only the type) of useCallback to allow for nullable functions
 export const useCallback = ucb as <T extends Function | null | undefined>(callback: NonNullable<T>, inputs: Inputs) => NonNullable<T>;
@@ -28,16 +32,16 @@ export const onfocusin = "onfocusin" satisfies keyof ElementProps<any>;
 // @ts-expect-error (Capitalizing these results in errors with at least grid navigation)
 export const onfocusout = "onfocusout" satisfies keyof ElementProps<any>;
 
-export type ElementProps<E extends EventTarget> = JSX.HTMLAttributes<E>;
-export type EventType<T extends EventTarget, E extends Event> = JSX.TargetedEvent<T, E>;
-export type FocusEventType<E extends EventTarget> = JSX.TargetedFocusEvent<E>;
-export type KeyboardEventType<E extends EventTarget> = JSX.TargetedKeyboardEvent<E>;
-export type MouseEventType<E extends EventTarget> = JSX.TargetedMouseEvent<E>;
-export type TouchEventType<E extends EventTarget> = JSX.TargetedTouchEvent<E>;
-export type PointerEventType<E extends EventTarget> = JSX.TargetedPointerEvent<E>;
-export type CompositionEventType<E extends EventTarget> = JSX.TargetedCompositionEvent<E>;
-export type DragEventType<E extends EventTarget> = JSX.TargetedDragEvent<E>;
-export type CSSProperties = JSX.CSSProperties;
+export type ElementProps<E extends EventTarget> = preact.HTMLAttributes<E>;
+export type EventType<T extends EventTarget, E extends Event> = preact.TargetedEvent<T, E>;
+export type FocusEventType<E extends EventTarget> = preact.TargetedFocusEvent<E>;
+export type KeyboardEventType<E extends EventTarget> = preact.TargetedKeyboardEvent<E>;
+export type MouseEventType<E extends EventTarget> = preact.TargetedMouseEvent<E>;
+export type TouchEventType<E extends EventTarget> = preact.TargetedTouchEvent<E>;
+export type PointerEventType<E extends EventTarget> = preact.TargetedPointerEvent<E>;
+export type CompositionEventType<E extends EventTarget> = preact.TargetedCompositionEvent<E>;
+export type DragEventType<E extends EventTarget> = preact.TargetedDragEvent<E>;
+export type CSSProperties = preact.CSSProperties;
 export type VNode = JSX.Element;
 
 
